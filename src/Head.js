4ed3923c2a7c85edecc6015ff97001d38ab7fcd8
RSwebdevelop <ranjan.webdeveloper@gmail.com>
2023-01-08 22:20:29 +0530

    react_learning

---
+++ src/Head.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Head(){  // functional component
+    return(
+     <div>
+         <h1>Hello from Header</h1>
+     </div>
+    )
+  }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Head from './Head';
+import Foot from './Foot';
+import Comp1 from './Comp1';
+import Counter, { AutoCounter, AutoCounter2 } from './Counter';
+
+
+function Button(props){
+    console.log(props);
+
+    // function handleClick(){
+    //     alert(props.data);
+    // }
+    return(
+        <div>
+            <button onClick={props.handleClick}>{props.text}</button>
+        </div>
+    )
+}
+
+function button1Click(){
+    console.log('clicked button 1')
+}
+
+function button2Click(){
+    console.log('clicked button 2')
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( 
+    <div>
+     <Head></Head>
+     <Button text ={"click from root"} val ={100} data = {'Button from root. Click to go home'} handleClick={button1Click} ></Button>
+     <Button text ={"click from root button 2"} val ={100} data = {'Button from root. Click to go products'} handleClick={button2Click}></Button>
+     <Foot></Foot>
+     <Comp1></Comp1>
+     <Counter/>
+    <AutoCounter/>
+    <AutoCounter2/>
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
